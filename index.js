d7ca2c06141d4c6f1aff17bddd00d5cfdffc5daf
minchang23 <142557712+minchang23@users.noreply.github.com>
2025-03-17 14:30:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 exports.handler = async function (event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  console.log("Request: \n" + JSON.stringify(event, null, 2));
   const name = event.name || "there"
   const message = `hi '${name}'. we are ale!!!!!!!!!!!!!!!!!!!!`
+  console.log("Response: \n" + message);
   return message;
 };
