3ecfba98d930930d7ddf41605d206eda624c3e28
minchang23 <142557712+minchang23@users.noreply.github.com>
2025-03-18 11:45:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ exports.handler = async function (event, context) {
   let name = "there";
 
   if (event.body) {
+    console.log("Request: \n" + JSON.stringify(event.body));  
     const body = JSON.parse(event.body);
-    console.log("Request: \n" + body);  
+    
     name = body.name || name;
   } else if (event.queryStringParameters) {
     console.log("Request: \n" + event.queryStringParameters);  
@@ -16,8 +17,8 @@ exports.handler = async function (event, context) {
 
   const response = {
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: `hi, ${name}. we are ale!!!`}),
+    body: JSON.stringify({ data: `hi, ${name}. we are ale!!!`}),
   }
-  console.log("Response: \n" + response);
+  console.log("Response: \n" + JSON.stringify(response.data));
   return response;
 };
