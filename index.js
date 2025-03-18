133fdd14de03a1c1add4369c474f9744792d7f8a
minchang23 <142557712+minchang23@users.noreply.github.com>
2025-03-18 11:12:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 exports.handler = async function (event, context) {
-  console.log("Request: \n" + JSON.stringify(event, null, 2));
-  const name = event.name || "there"
-  const message = `hi '${name}'. we are ale!!!!!!!!!!!!!!!!!!!!`
-  console.log("Response: \n" + message);
+  //console.log("Request: \n" + JSON.stringify(event, null, 2));
+  const body = json.loads(event["body"]);
+  console.log("Request: \n" + body);
+  const name = body.get("name", "there");
+  const res = {"message" : `hi '${name}'. we are ale!!!!!!!!!!!!!!!!!!!!`}
+  console.log("Response: \n" + res);
   return message;
 };
